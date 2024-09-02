8c63b43b0b295104e78b7ba26514774cb2d15627
mathi970 <cuki.mallqui.perro@gmail.com>
2024-09-02 10:42:44 -0500

    palabras nuevas

---
+++ ddpn.py
@@ -0,0 +1,15 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            }
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+    # ¿Qué debemos hacer si se encuentra la palabra?
+    
+else:
+    print("esa palabra no esa en nuestro diccioneario")
+    # ¿Qué hacer si no se encuentra la palabra?
+ 
+
